983bfe787fe4f62df2c0ddb653c492aeb1132f47
Arslan khan <Arslankhan2029@gmail.com>
2024-01-22 19:40:30 +0500

    Integrate Pagination Feature
    Description: "Introduced pagination functionality to efficiently handle large datasets, enhancing the user experience by breaking down content into manageable pages. This commit ensures smoother navigation through the application, particularly in scenarios with extensive product listings.

--- src/features/products/ProductSlice.js
+++ src/features/products/ProductSlice.js
@@ -1,60 +1,60 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchAllProducts, fetchProductsByfilter } from './ProductAPI';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchAllProducts, fetchProductsByFilters } from "./ProductAPI";
 
 const initialState = {
   products: [],
-  status: 'idle',
+  status: "idle",
+  totalItems: 0,
 };
 
 export const fetchAllProductsAsync = createAsyncThunk(
-  'product/fetchAllProducts',
+  "product/fetchAllProducts",
   async () => {
     const response = await fetchAllProducts();
-    // console.log('Fetched products:', response.data); // Log the fetched data
+    // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
 );
-export const fetchProductsByfilterAsync = createAsyncThunk(
-  'product/fetchProductsByfilter',
-  async ({filter,sort}) => {
-    const response = await fetchProductsByfilter(filter,sort);
-    // console.log('Fetched products:', response.data); // Log the fetched data
+export const fetchProductsByFiltersAsync = createAsyncThunk(
+  "product/fetchProductsByFilters",
+  async ({ filter, sort, pagination }) => {
+    const response = await fetchProductsByFilters(filter, sort, pagination);
+    // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
-); 
+);
 
 export const productSlice = createSlice({
-  name: 'product',
+  name: "product",
   initialState,
   reducers: {
     increment: (state) => {
-      // console.log('Incrementing value:', state.value); // Log the value before incrementing
       state.value += 1;
     },
   },
   extraReducers: (builder) => {
     builder
       .addCase(fetchAllProductsAsync.pending, (state) => {
-        // console.log('Fetching products...');
-        state.status = 'loading';
+        state.status = "loading";
       })
       .addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
-        // console.log('Products fetched successfully:', action.payload); // Log the received products
-        state.status = 'idle';
+        state.status = "idle";
         state.products = action.payload;
       })
-      .addCase(fetchProductsByfilterAsync.pending, (state) => {
-        // console.log('Fetching products...');
-        state.status = 'loading';
+      .addCase(fetchProductsByFiltersAsync.pending, (state) => {
+        state.status = "loading";
       })
-      .addCase(fetchProductsByfilterAsync.fulfilled, (state, action) => {
-        // console.log('Products fetched successfully:', action.payload); // Log the received products
-        state.status = 'idle';
-        state.products = action.payload;
+      .addCase(fetchProductsByFiltersAsync.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.products = action.payload.products;
+        state.totalItems = action.payload.totalItems;
       });
   },
 });
 
 export const { increment } = productSlice.actions;
-export const selectAllProducts = state=>state.product.products;
+
+export const selectAllProducts = (state) => state.product.products;
+export const selectTotalItems = (state) => state.product.totalItems;
+
 export default productSlice.reducer;
